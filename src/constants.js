eb193acb906fbdbfbb0a6a856aa979a3a86a4de9
Yong Loon <nyl9488.yln@gmail.com>
2024-11-13 23:49:32 +0800

    init

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+//BRAND
+export const name = 'CourseForge';
+export const company = 'CourseForge';
+export const websiteURL = 'http://localhost:3000';
+export const serverURL = 'http://localhost:5000';
+export const logo = 'https://courseforge.xyz/logo192.png';
+export const razorpayEnabled = false;
+export const paypalEnabled = false;
+export const stripeEnabled = true;
+export const paystackEnabled = false;
+
+//PRICING :-
+
+//FREE 
+export const FreeType = 'Free Plan';
+export const FreeCost = 0;
+export const FreeTime = '';
+
+//MONTHLY 
+export const MonthType = 'Monthly Plan';
+export const MonthCost = 9;
+export const MonthTime = 'month';
+
+//YEARLY 
+export const YearType = 'Yearly Plan';
+export const YearCost = 99;
+export const YearTime = 'year';
+
+//TESTIMONIAL
+export const review = "The AI Course Generator revolutionized my content creation process, providing accurate and relevant topics effortlessly. It's a time-saving powerhouse that enhances the quality and relevance of my courses. A must-have tool for educators seeking efficiency and impactful online learning experiences.";
+export const from = "Johnny Doe";
+export const profession = 'CEO';
+export const photoURL = 'https://images.unsplash.com/photo-1590895340509-793cb98788c9?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2hpdGUlMjBndXl8ZW58MHx8MHx8fDA%3D';
+
+//PAYPAL
+export const paypalPlanIdOne = "";
+export const paypalPlanIdTwo = "";
+
+//RAZORPAY
+export const razorpayKeyId = "";
+export const razorpayPlanIdOne = "";
+export const razorpayPlanIdTwo = "";
+
+//STRIPE
+export const stripePlanIdOne = "price_1QERZOCCYa6cAnBn8P8r6xR9";
+export const stripePlanIdTwo = "price_1QERZpCCYa6cAnBnXXlYiqR0";
+
+//PAYSTACK
+export const paystackPlanIdOne = "";
+export const paystackPlanIdTwo = "";
+export const amountInZarOne = '170';
+export const amountInZarTwo = '1871';
